37b8d1dc737d78cbfd2834f2bbf6950f64f72961
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-28 12:10:41 -0300

    enable viewBinding

--- app/src/main/java/com/vitoraguiardf/bobinabancking/MainActivity.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabancking/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.vitoraguiardf.bobinabancking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var view: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        view = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(view.root)
+        ViewCompat.setOnApplyWindowInsetsListener(view.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets

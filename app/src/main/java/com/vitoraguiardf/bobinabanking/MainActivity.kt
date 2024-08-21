d5535e7307675a1cd54906ec4b209be398cb6792
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-08-21 16:35:24 -0300

    update main activity

--- app/src/main/java/com/vitoraguiardf/bobinabanking/MainActivity.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/MainActivity.kt
@@ -1,25 +1,10 @@
 package com.vitoraguiardf.bobinabanking
 
-import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.vitoraguiardf.bobinabanking.databinding.ActivityMainBinding
 
-class MainActivity : CustomActivity() {
-    private lateinit var view: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        view = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(view.root)
-        ViewCompat.setOnApplyWindowInsetsListener(view.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        // Show singleton instance on start app
-        Toast.makeText(this, singleton.toString(), Toast.LENGTH_SHORT).show()
+class MainActivity : AbstractCustomActivity<ActivityMainBinding>() {
+    private lateinit var viewModel: MainViewModel
+    override fun viewBindingInflate(): ActivityMainBinding {
+        return ActivityMainBinding.inflate(layoutInflater)
     }
 }
---
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.vitoraguiardf.bobinabanking
+
+import com.vitoraguiardf.bobinabanking.utils.viewmodel.ViewModel
+
+class MainViewModel: ViewModel<Int, String, Void>()

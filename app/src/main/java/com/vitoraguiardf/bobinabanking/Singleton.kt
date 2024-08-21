c107b6fff35478b2cea2d48d6a1f92d7b96d584f
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-08-21 16:54:00 -0300

    singleton executor and handler

--- app/src/main/java/com/vitoraguiardf/bobinabanking/Singleton.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/Singleton.kt
@@ -1,10 +1,16 @@
 package com.vitoraguiardf.bobinabanking
 
 import android.app.Application
+import android.os.Looper
+import androidx.core.os.HandlerCompat
 import com.vitoraguiardf.bobinabanking.data.rest.RestClient
 import retrofit2.Retrofit
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class Singleton: Application() {
+    val executor: ExecutorService = Executors.newFixedThreadPool(4)
+    val handler = HandlerCompat.createAsync(Looper.getMainLooper())
     val retrofit: Retrofit = RestClient(this).retrofit
 
     override fun onCreate() {

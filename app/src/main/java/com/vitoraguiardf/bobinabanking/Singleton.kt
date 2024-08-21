aca1a4c499f0e905f917540ffe021b6b62988ecb
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-08-21 16:43:21 -0300

    singleton retrofit client

--- app/src/main/java/com/vitoraguiardf/bobinabanking/Singleton.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/Singleton.kt
@@ -1,8 +1,12 @@
 package com.vitoraguiardf.bobinabanking
 
 import android.app.Application
+import com.vitoraguiardf.bobinabanking.data.rest.RestClient
+import retrofit2.Retrofit
 
 class Singleton: Application() {
+    val retrofit: Retrofit = RestClient(this).retrofit
+
     override fun onCreate() {
         super.onCreate()
         _instance = _instance ?: this

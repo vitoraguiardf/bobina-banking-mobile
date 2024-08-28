7a04e9e402cfeb5549fa24b25ccf5a2cef27f003
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-08-28 17:01:16 -0300

    progress layout

--- app/src/main/java/com/vitoraguiardf/bobinabanking/ui/main/MainActivity.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/main/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.vitoraguiardf.bobinabanking.ui.main
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.vitoraguiardf.bobinabanking.R
-import com.vitoraguiardf.bobinabanking.utils.activities.CustomActivity
 import com.vitoraguiardf.bobinabanking.databinding.ActivityMainBinding
 import com.vitoraguiardf.bobinabanking.ui.home.HomeActivity
 import com.vitoraguiardf.bobinabanking.ui.login.LoginActivity
+import com.vitoraguiardf.bobinabanking.utils.activities.CustomActivity
 import com.vitoraguiardf.bobinabanking.utils.viewmodel.FormState
 
 class MainActivity : CustomActivity<ActivityMainBinding>() {
@@ -20,33 +20,37 @@ class MainActivity : CustomActivity<ActivityMainBinding>() {
         return ActivityMainBinding.inflate(layoutInflater)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.form.state.observe(this@MainActivity, Observer {
             val state = it?: return@Observer
-            when(state) {
-                FormState.SUCCESS -> {
-                    setLoadingState(false)
-                    return@Observer
-                }
-                FormState.FAILED -> {
-                    setLoadingState(false)
-                    return@Observer
-                }
-                FormState.RUNNING -> {
-                    setLoadingState(true)
-                    return@Observer
-                }
+
+            binding.actionSignInContinue.visibility = when(state) {
+                FormState.SUCCESS -> View.VISIBLE
+                else -> View.GONE
             }
+
+            binding.actionSignIn.visibility = when(state) {
+                FormState.FAILED -> View.VISIBLE
+                else -> View.GONE
+            }
+
+            binding.actionSignUp.visibility = when(state) {
+                FormState.FAILED -> View.VISIBLE
+                else -> View.GONE
+            }
+
+            binding.includedProgress.loading.visibility = when(state) {
+                FormState.RUNNING -> View.VISIBLE
+                else -> View.GONE
+            }
+
         })
         viewModel.form.result.observe(this@MainActivity, Observer {
             val user = it?: return@Observer
-            Toast.makeText(this@MainActivity, "Bem vindo ${user.name}!", Toast.LENGTH_LONG).show()
-            binding.actionSignIn.visibility = View.GONE
-            binding.actionSignUp.visibility = View.GONE
             binding.actionSignInContinue.text = "Continuar como ${user.name}!"
-            binding.actionSignInContinue.visibility = View.VISIBLE
         })
         viewModel.form.throwable.observe(this@MainActivity, Observer {
             val throwable = it?: return@Observer
@@ -61,7 +65,7 @@ class MainActivity : CustomActivity<ActivityMainBinding>() {
         }
         binding.actionSignUp.visibility = View.GONE
         binding.actionSignInContinue.visibility = View.GONE
-
+        verifySession()
     }
 
     override fun onPostResume() {
@@ -72,15 +76,7 @@ class MainActivity : CustomActivity<ActivityMainBinding>() {
     private fun verifySession() {
         if (instance.token != null) {
             viewModel.me()
-        } else {
-            setLoadingState(false)
         }
-    }
-
-    private fun setLoadingState(loading: Boolean) {
-        binding.actionSignIn.isEnabled = !loading
-        binding.actionSignIn.text = if (!loading) getString(R.string.action_sign_in) else getString(R.string.loading)
-        binding.loading.visibility = if (!loading) View.GONE else View.VISIBLE
     }
 
 }

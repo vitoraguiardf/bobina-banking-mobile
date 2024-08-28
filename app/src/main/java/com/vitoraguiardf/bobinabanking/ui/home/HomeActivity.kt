fe74a9aa12f0e2789d35202407840295e59da614
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-08-28 18:19:29 -0300

    update home view

--- app/src/main/java/com/vitoraguiardf/bobinabanking/ui/home/HomeActivity.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.vitoraguiardf.bobinabanking.ui.home
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -8,6 +9,7 @@ import com.vitoraguiardf.bobinabanking.Singleton
 import com.vitoraguiardf.bobinabanking.databinding.ActivityHomeBinding
 import com.vitoraguiardf.bobinabanking.ui.adapters.TransactionAdapter
 import com.vitoraguiardf.bobinabanking.utils.activities.CustomActivity
+import com.vitoraguiardf.bobinabanking.utils.viewmodel.FormState
 
 class HomeActivity : CustomActivity<ActivityHomeBinding>() {
     private lateinit var viewModel: HomeViewModel
@@ -19,20 +21,25 @@ class HomeActivity : CustomActivity<ActivityHomeBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        viewModel.form.state.observe(this, Observer {
+            val state = it?: return@Observer
+            binding.included.loading.visibility = when(state) {
+                FormState.RUNNING -> View.VISIBLE
+                else -> View.GONE
+            }
+            binding.recyclerViewLancamentos.visibility = when(state) {
+                FormState.SUCCESS -> View.VISIBLE
+                else -> View.GONE
+            }
+        })
         viewModel.form.throwable.observe(this, Observer {
             val throwable = it?: return@Observer
-            throwable.printStackTrace()
             Toast.makeText(this, throwable.message, Toast.LENGTH_LONG).show()
         })
-
         viewModel.form.result.observe(this, Observer {
             val transactions = it?: return@Observer
-
             val adapter = TransactionAdapter(this, transactions)
-
             binding.recyclerViewLancamentos.adapter = adapter
-
-            Toast.makeText(this, "${transactions.size} Transações!", Toast.LENGTH_LONG).show()
         })
 
         binding.textViewUserName.text = Singleton.instance.user.name

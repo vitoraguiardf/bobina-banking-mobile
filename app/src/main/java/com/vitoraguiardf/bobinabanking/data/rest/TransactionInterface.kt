eb90501eda151523b9f7bb9818910419330ea944
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-08-29 11:55:19 -0300

    addind resume method on transaction repository

--- app/src/main/java/com/vitoraguiardf/bobinabanking/data/rest/TransactionInterface.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/data/rest/TransactionInterface.kt
@@ -1,10 +1,14 @@
 package com.vitoraguiardf.bobinabanking.data.rest
 
 import com.vitoraguiardf.bobinabanking.data.entity.Transaction
+import com.vitoraguiardf.bobinabanking.data.entity.User
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface TransactionInterface {
+
+    @GET("bobina-banking/resume")
+    fun resume(): Call<User>
 
     @GET("bobina-banking/transactions")
     fun transactions(): Call<Array<Transaction>>
--- app/src/main/java/com/vitoraguiardf/bobinabanking/data/rest/TransactionRepository.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/data/rest/TransactionRepository.kt
@@ -2,10 +2,15 @@ package com.vitoraguiardf.bobinabanking.data.rest
 
 import com.vitoraguiardf.bobinabanking.Singleton
 import com.vitoraguiardf.bobinabanking.data.entity.Transaction
+import com.vitoraguiardf.bobinabanking.data.entity.User
 
 class TransactionRepository: AbstractRestRepository() {
 
     private val retrofit = Singleton.instance.retrofit.create(TransactionInterface::class.java)
+
+    suspend fun resume(): Result<User> {
+        return execute(retrofit.resume())
+    }
 
     suspend fun transactions(): Result<Array<Transaction>> {
         return execute(retrofit.transactions())

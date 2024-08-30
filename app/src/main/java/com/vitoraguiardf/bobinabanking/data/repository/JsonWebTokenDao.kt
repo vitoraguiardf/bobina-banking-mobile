eb7b48621e86054e9fff8a346466b821719ae647
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-08-30 11:17:26 -0300

    fix

--- app/src/main/java/com/vitoraguiardf/bobinabanking/data/repository/JsonWebTokenDao.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/data/repository/JsonWebTokenDao.kt
@@ -7,9 +7,9 @@ import com.vitoraguiardf.bobinabanking.data.entity.JsonWebToken
 @Dao
 interface JsonWebTokenDao: DaoRepository<JsonWebToken, Int> {
     @Query("SELECT * FROM JsonWebToken")
-    suspend fun findAll(): List<JsonWebToken>
+    fun findAll(): List<JsonWebToken>
     @Query("SELECT * FROM JsonWebToken ORDER BY ID DESC LIMIT 1")
-    suspend fun findLast(): JsonWebToken
+    fun findLast(): JsonWebToken?
     @Query("SELECT * FROM JsonWebToken WHERE id IN (:ids)")
-    suspend fun findAllById(ids: IntArray): List<JsonWebToken>
+    fun findAllById(ids: IntArray): List<JsonWebToken>
 }

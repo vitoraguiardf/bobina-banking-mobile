b7a1135212a0815113ffde7f434708ee7bdc2d8b
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-03 17:20:57 -0300

    custom binding activity impl

--- app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/CoilTransactionActivity.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/CoilTransactionActivity.kt
@@ -1,19 +1,24 @@
 package com.vitoraguiardf.bobinabanking.ui.transaction
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.vitoraguiardf.bobinabanking.R
+import com.vitoraguiardf.bobinabanking.databinding.ActivityCoilTransactionBinding
 import com.vitoraguiardf.bobinabanking.ui.transaction.fragments.CoilTransactionFragment
+import com.vitoraguiardf.bobinabanking.utils.activities.CustomActivity
 
-class CoilTransactionActivity : AppCompatActivity() {
+class CoilTransactionActivity : CustomActivity<ActivityCoilTransactionBinding>() {
+
+    override fun viewBindingInflate(): ActivityCoilTransactionBinding {
+        return ActivityCoilTransactionBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coil_transaction)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, CoilTransactionFragment.newInstance())
                 .commitNow()
         }
     }
+
 }

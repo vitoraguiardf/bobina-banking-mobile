78a36806e4c26e0af7eabbef7e43a1ed36fb945e
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-04 12:13:31 -0300

    confirmation fragment

---
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/fragments/ConfirmationFragment.kt
@@ -0,0 +1,31 @@
+package com.vitoraguiardf.bobinabanking.ui.transaction.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.vitoraguiardf.bobinabanking.R
+
+class ConfirmationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ConfirmationFragment()
+    }
+
+    private val viewModel: ConfirmationViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+}
---
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/fragments/ConfirmationViewModel.kt
@@ -0,0 +1,7 @@
+package com.vitoraguiardf.bobinabanking.ui.transaction.fragments
+
+import androidx.lifecycle.ViewModel
+
+class ConfirmationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

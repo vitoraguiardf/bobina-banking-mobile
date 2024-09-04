c4998d71d5c8e5a37ca3abcf2fa44b199352b7d0
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-04 12:12:48 -0300

    qr code scan fragment

---
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/fragments/QrCodeScanViewModel.kt
@@ -0,0 +1,7 @@
+package com.vitoraguiardf.bobinabanking.ui.transaction.fragments
+
+import androidx.lifecycle.ViewModel
+
+class QrCodeScanViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

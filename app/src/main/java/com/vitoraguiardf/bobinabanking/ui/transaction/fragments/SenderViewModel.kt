662c2e23bae49c16b3ac1e24aaf9d10c7da45cdf
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-06 11:01:33 -0300

    rm sender fragment

--- app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/fragments/SenderViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.vitoraguiardf.bobinabanking.ui.transaction.fragments
-
-import androidx.lifecycle.ViewModel
-
-class SenderViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}

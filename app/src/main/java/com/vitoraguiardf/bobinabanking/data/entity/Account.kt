a92a5c3881fd60a21f5d953f083d927583dcb98c
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-09 16:56:29 -0300

    update account model

--- app/src/main/java/com/vitoraguiardf/bobinabanking/data/entity/Account.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/data/entity/Account.kt
@@ -2,10 +2,17 @@ package com.vitoraguiardf.bobinabanking.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity
 data class Account(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
     val name: String,
-)
+    @SerializedName("holder_name")
+    val holderName: String,
+) {
+    override fun toString(): String {
+        return "$id: $name"
+    }
+}

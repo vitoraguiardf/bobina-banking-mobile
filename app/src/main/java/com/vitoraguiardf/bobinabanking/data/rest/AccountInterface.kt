2303ac5fa35e4688cd58c206689845e7011b4d63
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-09 16:54:59 -0300

    fix query retrofit annotation

--- app/src/main/java/com/vitoraguiardf/bobinabanking/data/rest/AccountInterface.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/data/rest/AccountInterface.kt
@@ -3,10 +3,15 @@ package com.vitoraguiardf.bobinabanking.data.rest
 import com.vitoraguiardf.bobinabanking.data.entity.Account
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface AccountInterface {
 
     @GET("bobina-banking/accounts")
     fun get(): Call<Array<Account>>
+
+    @POST("bobina-banking/accounts/search")
+    fun search(@Query("email") email: String): Call<Array<Account>>
 
 }

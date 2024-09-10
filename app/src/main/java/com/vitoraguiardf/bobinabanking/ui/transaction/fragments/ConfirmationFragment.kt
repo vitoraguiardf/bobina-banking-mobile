b4cd719e3238d4b749faa4ef60abff736998cc04
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-10 16:49:04 -0300

    update confirmation fragment

--- app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/fragments/ConfirmationFragment.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/transaction/fragments/ConfirmationFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.vitoraguiardf.bobinabanking.R
 import com.vitoraguiardf.bobinabanking.databinding.FragmentConfirmationBinding
 import com.vitoraguiardf.bobinabanking.ui.ViewModelFactory
 import com.vitoraguiardf.bobinabanking.ui.transaction.SharedViewModel
@@ -32,6 +33,16 @@ class ConfirmationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentConfirmationBinding.inflate(inflater, container, false)
+        val recipientAccount = sharedModel.transferenceForm.recipient.value
+        val quantity = sharedModel.transferenceForm.quantity.value
+        recipientAccount?.let {
+            binding.username.text = it.holderName
+            binding.accountName.text = it.name
+        }
+        quantity?.let {
+            binding.quantity.text = String.format("%s %s",
+                it, getString(R.string.unities))
+        }
         return binding.root
     }
 }

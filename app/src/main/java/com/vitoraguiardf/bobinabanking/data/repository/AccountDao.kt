d865f57242c6dc85c5345232dffb99000422dc8e
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-11 10:04:21 -0300

    add query

--- app/src/main/java/com/vitoraguiardf/bobinabanking/data/repository/AccountDao.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/data/repository/AccountDao.kt
@@ -1,7 +1,13 @@
 package com.vitoraguiardf.bobinabanking.data.repository
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
 import com.vitoraguiardf.bobinabanking.data.entity.Account
 
 @Dao
-interface AccountDao: DaoRepository<Account, Int>
+interface AccountDao: DaoRepository<Account, Int> {
+    @Transaction
+    @Query("SELECT * FROM Account ORDER BY ID DESC")
+    fun findAll(): Array<Account>
+}

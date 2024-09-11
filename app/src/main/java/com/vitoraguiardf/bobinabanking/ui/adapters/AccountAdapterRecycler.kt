80bdcbf73df3963608ce5cf88f3843f267293527
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-09-11 10:06:42 -0300

    account card layout

--- app/src/main/java/com/vitoraguiardf/bobinabanking/ui/adapters/AccountAdapterRecycler.kt
+++ app/src/main/java/com/vitoraguiardf/bobinabanking/ui/adapters/AccountAdapterRecycler.kt
@@ -1,22 +1,21 @@
 package com.vitoraguiardf.bobinabanking.ui.adapters
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.vitoraguiardf.bobinabanking.data.entity.Account
-import com.vitoraguiardf.bobinabanking.databinding.LayoutTransactionItemBinding
+import com.vitoraguiardf.bobinabanking.databinding.CardAccountBinding
 import com.vitoraguiardf.bobinabanking.utils.viewbinding.AbstractRecyclerViewAdapter
 
 class AccountAdapterRecycler(context: Context, items: Array<Account>):
-    AbstractRecyclerViewAdapter<Account, LayoutTransactionItemBinding>(context, items) {
-    override fun viewHolder(parent: ViewGroup?): ViewBinderHolder<Account, LayoutTransactionItemBinding> {
-        val binder = LayoutTransactionItemBinding.inflate(
+    AbstractRecyclerViewAdapter<Account, CardAccountBinding>(context, items) {
+    override fun viewHolder(parent: ViewGroup?): ViewBinderHolder<Account, CardAccountBinding> {
+        val binder = CardAccountBinding.inflate(
             LayoutInflater.from(parent!!.context), parent, false)
-        return object: ViewBinderHolder<Account, LayoutTransactionItemBinding>(binder) {
-            @SuppressLint("SetTextI18n", "UseCompatLoadingForDrawables")
+        return object: ViewBinderHolder<Account, CardAccountBinding>(binder) {
             override fun bind(item: Account) {
-                binder.textViewType.text = item.toString()
+                binder.username.text = item.holderName.uppercase()
+                binder.account.text = item.name.uppercase()
             }
         }
     }
